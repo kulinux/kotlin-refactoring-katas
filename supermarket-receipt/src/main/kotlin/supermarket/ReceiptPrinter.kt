c4b03298aa0938457e0a2d4fdd87b53c27e2feb2
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-09-09 09:03:02 +0200

    Still with ReceiptPrinter

--- supermarket-receipt/src/main/kotlin/supermarket/ReceiptPrinter.kt
+++ supermarket-receipt/src/main/kotlin/supermarket/ReceiptPrinter.kt
@@ -1,61 +1,61 @@
 package supermarket
 
+import supermarket.model.Discount
 import supermarket.model.Receipt
 import supermarket.model.ReceiptItem
 import java.util.*
 
+private const val CARRIAGE_RETURN = "\n"
+
 class ReceiptPrinter @JvmOverloads constructor(private val columns: Int = 40) {
   fun printReceipt(receipt: Receipt): String {
-    return handleItems(receipt) +
-      handleDiscounts(receipt) +
-      handleTotal(receipt)
+    return handleItems(receipt.getItems()) +
+      handleDiscounts(receipt.getDiscounts()) +
+      handleTotal(receipt.totalPrice)
   }
 
-  private fun handleTotal(receipt: Receipt): String {
-    val result = StringBuilder()
-    result.append("\n")
-    val pricePresentation = String.format(Locale.UK, "%.2f", receipt.totalPrice as Double)
+  private fun handleItems(items: List<ReceiptItem>): String {
+    return items.joinToString("") { handleItem(it) }
+  }
+
+  private fun handleDiscounts(discounts: List<Discount>): String {
+    return discounts.joinToString("") { handleDiscount(it) }
+  }
+
+  private fun handleTotal(totalPrice: Double): String {
+    val pricePresentation = String.format(Locale.UK, "%.2f", totalPrice)
     val total = "Total: "
-    val whitespace = getWhitespace(this.columns - total.length - pricePresentation.length)
-    result.append(total).append(whitespace).append(pricePresentation)
-    return result.toString()
+    val whitespaces = whiteSpacesOfLength(this.columns - total.length - pricePresentation.length)
+
+    return "$CARRIAGE_RETURN$total$whitespaces$pricePresentation"
   }
 
-  private fun handleDiscounts(receipt: Receipt): String {
-    val result = StringBuilder()
-    for (discount in receipt.getDiscounts()) {
-      val productPresentation = discount.product.name
-      val pricePresentation = String.format(Locale.UK, "%.2f", discount.discountAmount)
-      val description = discount.description
-      result.append(description)
-      result.append("(")
-      result.append(productPresentation)
-      result.append(")")
-      result.append(getWhitespace(this.columns - 3 - productPresentation.length - description.length - pricePresentation.length))
-      result.append("-")
-      result.append(pricePresentation)
-      result.append("\n")
-    }
-    return result.toString()
+
+  private fun handleDiscount(discount: Discount): String {
+    val productPresentation = discount.product.name
+    val pricePresentation = String.format(Locale.UK, "%.2f", discount.discountAmount)
+    val description = discount.description
+
+    val whiteSpacesOfLength =
+      whiteSpacesOfLength(this.columns - 3 - productPresentation.length - description.length - pricePresentation.length)
+
+    return "$description($productPresentation)$whiteSpacesOfLength-$pricePresentation$CARRIAGE_RETURN"
   }
 
-  private fun handleItems(receipt: Receipt): String {
-    val result = StringBuilder()
-    for (item in receipt.getItems()) {
-      val price = String.format(Locale.UK, "%.2f", item.totalPrice)
-      val quantity = presentQuantity(item)
-      val name = item.product.name
-      val unitPrice = String.format(Locale.UK, "%.2f", item.price)
 
-      val whitespaceSize = this.columns - name.length - price.length
-      var line = name + getWhitespace(whitespaceSize) + price + "\n"
+  private fun handleItem(item: ReceiptItem): String {
+    val price = String.format(Locale.UK, "%.2f", item.totalPrice)
+    val quantity = presentQuantity(item)
+    val name = item.product.name
+    val unitPrice = String.format(Locale.UK, "%.2f", item.price)
 
-      if (item.quantity != 1.0) {
-        line += "  $unitPrice * $quantity\n"
-      }
-      result.append(line)
+    val whitespaceSize = this.columns - name.length - price.length
+    val baseLine = name + whiteSpacesOfLength(whitespaceSize) + price + "$CARRIAGE_RETURN"
+
+    if (item.quantity != 1.0) {
+      return "$baseLine  $unitPrice * $quantity$CARRIAGE_RETURN"
     }
-    return result.toString()
+    return baseLine
   }
 
   private fun presentQuantity(item: ReceiptItem): String {
@@ -65,7 +65,7 @@ class ReceiptPrinter @JvmOverloads constructor(private val columns: Int = 40) {
       String.format(Locale.UK, "%.3f", item.quantity)
   }
 
-  private fun getWhitespace(whitespaceSize: Int): String {
+  private fun whiteSpacesOfLength(whitespaceSize: Int): String {
     val whitespace = StringBuilder()
     for (i in 0 until whitespaceSize) {
       whitespace.append(" ")
