bf0ffc2698988db29dd43db42327fdbe1ad463d0
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2023-09-16 08:16:53 +0200

    Bundle works for only one of each

--- supermarket-receipt/src/main/kotlin/supermarket/model/Offer.kt
+++ supermarket-receipt/src/main/kotlin/supermarket/model/Offer.kt
@@ -3,35 +3,22 @@ package supermarket.model
 abstract class Offer protected constructor(
   internal val products: Set<Product>, internal var argument: Double
 ) {
-  open fun discount(unitPrice: Double, quantity: Double): Discount? {
-    return null
-  }
-  open fun discount(catalog: SupermarketCatalog, productQuantities: Map<Product, Double>): Discount? {
-    for (productQuantity in productQuantities.entries) {
-      val product = productQuantity.key
-      val quantity = productQuantity.value
+  abstract fun discount(catalog: SupermarketCatalog, productQuantities: Map<Product, Double>): Discount?
 
-      if(products.contains(product) && products.size == 1) {
-        val unitPrice = catalog.getUnitPrice(product)
-        return discount(unitPrice, quantity)
-      }
-    }
-    return null
-  }
 
   companion object {
     fun build(offerType: SpecialOfferType, products: Set<Product>, argument: Double): Offer {
       if (offerType === SpecialOfferType.ThreeForTwo) {
-        return ThreeForTwo(products, argument)
+        return ThreeForTwo(products.first(), argument)
       }
       if (offerType === SpecialOfferType.TenPercentDiscount) {
-        return TenPercentDiscount(products, argument)
+        return TenPercentDiscount(products.first(), argument)
       }
       if (offerType === SpecialOfferType.TwoForAmount) {
-        return TwoForAmount(products, argument)
+        return TwoForAmount(products.first(), argument)
       }
       if (offerType === SpecialOfferType.FiveForAmount) {
-        return FiveForAmount(products, argument)
+        return FiveForAmount(products.first(), argument)
       }
       if (offerType === SpecialOfferType.Bundle) {
         return Bundle(products, argument)
@@ -39,13 +26,30 @@ abstract class Offer protected constructor(
       throw Error("No Offer Type for $offerType")
     }
   }
-
-
 }
 
-private class TwoForAmount(product: Set<Product>, argument: Double) : Offer(product, argument) {
 
-  override fun discount(unitPrice: Double, quantity: Double): Discount? {
+private abstract class OnProductOffer(product: Product, argument: Double) : Offer(setOf(product), argument) {
+  protected abstract fun discountOfSingleProduct(unitPrice: Double, quantity: Double): Discount?
+
+  override fun discount(catalog: SupermarketCatalog, productQuantities: Map<Product, Double>): Discount? {
+    for (productQuantity in productQuantities.entries) {
+      val product = productQuantity.key
+      val quantity = productQuantity.value
+
+      if (products.contains(product) && products.size == 1) {
+        val unitPrice = catalog.getUnitPrice(product)
+        return discountOfSingleProduct(unitPrice, quantity)
+      }
+    }
+    return null
+  }
+}
+
+
+private class TwoForAmount(product: Product, argument: Double) : OnProductOffer(product, argument) {
+
+  override fun discountOfSingleProduct(unitPrice: Double, quantity: Double): Discount? {
     val quantityAsInt = quantity.toInt()
     if (quantityAsInt >= 2) {
       val total = argument * (quantityAsInt / 2) + quantityAsInt % 2 * unitPrice
@@ -56,16 +60,16 @@ private class TwoForAmount(product: Set<Product>, argument: Double) : Offer(prod
   }
 }
 
-private class TenPercentDiscount(product: Set<Product>, argument: Double) : Offer(product, argument) {
+private class TenPercentDiscount(product: Product, argument: Double) : OnProductOffer(product, argument) {
 
-  override fun discount(unitPrice: Double, quantity: Double): Discount {
+  override fun discountOfSingleProduct(unitPrice: Double, quantity: Double): Discount {
     return Discount(products, "$argument% off", quantity * unitPrice * argument / 100.0)
   }
 }
 
-private class ThreeForTwo(product: Set<Product>, argument: Double) : Offer(product, argument) {
+private class ThreeForTwo(product: Product, argument: Double) : OnProductOffer(product, argument) {
 
-  override fun discount(unitPrice: Double, quantity: Double): Discount {
+  override fun discountOfSingleProduct(unitPrice: Double, quantity: Double): Discount {
     val quantityAsInt = quantity.toInt()
     val numberOfXs = quantityAsInt / 3
     val discountAmount =
@@ -74,9 +78,9 @@ private class ThreeForTwo(product: Set<Product>, argument: Double) : Offer(produ
   }
 }
 
-private class FiveForAmount(product: Set<Product>, argument: Double) : Offer(product, argument) {
+private class FiveForAmount(product: Product, argument: Double) : OnProductOffer(product, argument) {
 
-  override fun discount(unitPrice: Double, quantity: Double): Discount {
+  override fun discountOfSingleProduct(unitPrice: Double, quantity: Double): Discount {
     val quantityAsInt = quantity.toInt()
     val numberOfXs = quantityAsInt / 5
     val discountTotal = unitPrice * quantity - (argument * numberOfXs + quantityAsInt % 5 * unitPrice)
@@ -87,7 +91,9 @@ private class FiveForAmount(product: Set<Product>, argument: Double) : Offer(pro
 private class Bundle(products: Set<Product>, argument: Double) : Offer(products, argument) {
 
   override fun discount(catalog: SupermarketCatalog, productQuantities: Map<Product, Double>): Discount? {
-    return Discount(products, "not implemented",  10.0)
+    val totalPrice = productQuantities.map { (product, _) -> catalog.getUnitPrice(product) }.sum()
+    val discountTotal = totalPrice * argument / 100
+    return Discount(products, "not implemented", discountTotal)
   }
 }
 
--- supermarket-receipt/src/test/kotlin/supermarket/model/OfferTest.kt
+++ supermarket-receipt/src/test/kotlin/supermarket/model/OfferTest.kt
@@ -1,22 +1,37 @@
 package supermarket.model
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class OfferTest {
-  @Test
-  fun shouldDiscountAn10bundle() {
-    val toothbrush = Product.each("toothbrush")
-    val toothpaste = Product.each("toothpaste")
+  private val toothbrush = Product.each("toothbrush")
+  private val toothpaste = Product.each("toothpaste")
+  private val catalog = FakeCatalog()
 
-    val catalog = FakeCatalog()
+  @BeforeEach
+  private fun setupCatalog() {
     catalog.addProduct(toothbrush, 50.0)
     catalog.addProduct(toothpaste, 50.0)
+  }
 
+  @Test
+  fun shouldDiscountAn10bundle() {
     val offer = Offer.build(SpecialOfferType.Bundle, setOf(toothbrush, toothpaste), 10.0)
 
     val discount = offer.discount(catalog, mapOf(toothbrush to 1.0, toothpaste to 1.0))
 
     assertEquals(10.0, discount?.discountAmount)
   }
+
+  @Test
+  fun shouldDiscountAn5bundle() {
+    val offer = Offer.build(SpecialOfferType.Bundle, setOf(toothbrush, toothpaste), 5.0)
+
+    val discount = offer.discount(catalog, mapOf(toothbrush to 1.0, toothpaste to 1.0))
+
+    assertEquals(5.0, discount?.discountAmount)
+  }
+
+
 }
